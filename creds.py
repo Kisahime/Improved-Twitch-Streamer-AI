c2f7ad093b67b0aa6061aa399353bbecb43fed8a
Kisahime <120521433+Kisahime@users.noreply.github.com>
2023-04-27 17:27:51 +0100

    Delete creds.py

--- creds.py
+++
@@ -1,8 +0,0 @@
-# You're Twitch Token 
-TWITCH_TOKEN = "oauth:eqc298o4hbmkutnj8rfxanqti14g5v"
-# Your TWITCH Channel Name
-TWITCH_CHANNEL = "okatsu_arisa"
-# Your OpenAI API Key
-OPENAI_API_KEY = "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"
-# Your Google Cloud JSON Path
-GOOGLE_JSON_PATH = "concrete-envoy-382316-79e2fdc920fc.json"
